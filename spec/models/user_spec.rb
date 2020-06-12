4ae5aab67cfd79c460b9cc221b567fc8d8c36d6c
kitpao <kitpao91@gmail.com>
2020-06-12 17:45:20 -0500

    :white_check_mark: Add test for User model
    git push

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe Host do
+    it "has invitations" do
+      should have_many(:invitations).class_name('Invitation')
+    end
+  end
+=begin
+  host.invitations ->
+  invited.invitations
+  host.event
+=end
+.
 end
