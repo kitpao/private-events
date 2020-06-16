a184e4ec5b8300e671fba3979efd9c6531e706dc
kitpao <kitpao91@gmail.com>
2020-06-16 15:11:11 -0500

    :sparkles: Add user-invitations bidirectional relation

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,3 +1,4 @@
 class Invitation < ApplicationRecord
   belongs_to :event
+  belongs_to :host, class_name: "User"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_many :created_events, class_name: "Event", foreign_key: :host_id
-  
+  has_many :invitations, through: :created_events
+
+  #User.first.created_events.first.invitations
+
 end
