3c24fe1337d472501cfbb7e2e56e333da50acba6
Eduardo <jecruxiz@gmail.com>
2020-06-16 15:26:42 -0500

    :poop: Write bad code to test invited-invitations relation

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,4 +1,5 @@
 class Invitation < ApplicationRecord
   belongs_to :event
   belongs_to :host, class_name: "User"
+  belongs_to :invited, class_name: 'User'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 class User < ApplicationRecord
   has_many :created_events, class_name: "Event", foreign_key: :host_id
-  has_many :invitations, through: :created_events
-
-  #User.first.created_events.first.invitations
-
+  has_many :invitations, through: :created_events, source: :host
+  has_many :invitations, foreign_key: :invited_id
 end
