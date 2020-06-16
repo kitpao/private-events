9cef60d70d9d860bcc8111bd852551578806872d
kitpao <kitpao91@gmail.com>
2020-06-16 16:08:17 -0500

    :art: Fix intited-invitations association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_many :created_events, class_name: "Event", foreign_key: :host_id
-  has_many :invitations, through: :created_events, source: :host
-  has_many :invitations, foreign_key: :invited_id
+  has_many :created_invitations, through: :created_events, source: :invitations
+  has_many :received_invitations, class_name: "Invitation", foreign_key: :invited_id
 end
