86201f2a901b59d63f45d1cfd17b1bc75ec02e28
Eduardo <jecruxiz@gmail.com>
2020-06-16 16:22:13 -0500

    :sparkles: Add relation invited(user)-attended_events(event) relation
    'source:event' means who is attended_events

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   has_many :created_events, class_name: "Event", foreign_key: :host_id
   has_many :created_invitations, through: :created_events, source: :invitations
   has_many :received_invitations, class_name: "Invitation", foreign_key: :invited_id
+  has_many :attended_events, through: :received_invitations, source: :event 
 end
