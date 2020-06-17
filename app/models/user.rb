f6bbff797f7ed5b7954ddaa7260b6f692065541f
Eduardo <jecruxiz@gmail.com>
2020-06-17 12:26:01 -0500

    :sparkles: add validations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   has_many :created_invitations, through: :created_events, source: :invitations
   has_many :received_invitations, class_name: "Invitation", foreign_key: :invited_id
   has_many :attended_events, through: :received_invitations, source: :event
+
+  validates :name, presence: true, 
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 3, maximum: 25 }
 end
