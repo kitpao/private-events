4c7462da624941c55deb81a39992a489c9023f53
kitpao <kitpao91@gmail.com>
2020-06-16 19:17:32 -0500

    :sparkles: Add event-inviteds associations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,9 @@
 class Event < ApplicationRecord
   belongs_to :host, class_name: "User"
   has_many :invitations
+  has_many :inviteds, through: :invitations, source: :invited
+
+
+
+  #event1.invitations.first.invited
 end
