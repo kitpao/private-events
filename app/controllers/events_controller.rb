ef2f5f42d7cd5e3a0b182a21a025aeea0d4919c7
Eduardo <jecruxiz@gmail.com>
2020-06-18 12:16:20 -0500

    :sparkles: add index, show, new , create actions for events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,6 +2,29 @@ class EventsController < ApplicationController
   before_action :require_user, except: [:index, :show]
 
   def index
-    render json: { happy: 'Im the index '}
+    @events = Event.all
   end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.host = current_user.id
+
+    if @event.save
+      redirect_to  event_path(@event)
+    else
+      render :new
+    end
+  end
+  
+  private
+    def event_params
+      params.require(:event).permit(:description, :location, :date)
+    end
 end
