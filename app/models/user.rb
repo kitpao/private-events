2fdb29d92aa3bb3946eb9fc68b8f7ed0d16e98af
kitpao <kitpao91@gmail.com>
2020-06-18 15:32:16 -0500

    :sparkles: Add dependent destroys for user-events

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
-  has_many :created_events, class_name: "Event", foreign_key: :host_id
-  has_many :created_invitations, through: :created_events, source: :invitations
-  has_many :received_invitations, class_name: "Invitation", foreign_key: :invited_id
+  has_many :created_events, class_name: "Event", foreign_key: :host_id, dependent: :destroy
+  has_many :created_invitations, through: :created_events, source: :invitations, dependent: :destroy
+  has_many :received_invitations, class_name: "Invitation", foreign_key: :invited_id, dependent: :destroy
   has_many :attended_events, through: :received_invitations, source: :event
 
-  validates :name, presence: true, 
+  validates :name, presence: true,
             uniqueness: { case_sensitive: false },
             length: { minimum: 3, maximum: 25 }
 end
