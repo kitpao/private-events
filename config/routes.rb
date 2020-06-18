964d73bca2f09af5013ebac9eecf2efaf4d370d0
Eduardo <jecruxiz@gmail.com>
2020-06-18 12:54:57 -0500

    :lipstick: Update ui for every form

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get '/health', to: 'health#health'
   resources :users, except: %i[index update edit new]
   resources :sessions, only: %i[create destroy]
+  resources :events , except: %i[update edit destroy]
   get '/sign_in', to: 'sessions#new'
   get '/sign_up', to: 'users#new'
 
