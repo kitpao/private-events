68ce6a42276fbb60e76ca44355fd9894e8a36b4c
Eduardo <jecruxiz@gmail.com>
2020-06-19 17:04:00 -0500

    :white_check_mark: Update test for Invitation model

--- spec/models/invitation_spec.rb
+++ spec/models/invitation_spec.rb
@@ -2,6 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Invitation, type: :model do
   describe Invitation do
-    should belong_to()
+    should belong_to(:event)
+    should belong_to(:host).class_name('User')
+    should belong_to(:invited).class_name('User')
+
+    should validate_presence_of(:invited_id)
+    should validate_uniqueness_of(:event_id).scoped_to(:invited_id)
   end
 end
