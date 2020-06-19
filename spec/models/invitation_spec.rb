b6d307a23929d284c20c60123695efffdb5d6689
Eduardo <jecruxiz@gmail.com>
2020-06-19 17:08:32 -0500

    :bug: Fix tests for Invitation model

--- spec/models/invitation_spec.rb
+++ spec/models/invitation_spec.rb
@@ -2,11 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Invitation, type: :model do
   describe Invitation do
-    should belong_to(:event)
-    should belong_to(:host).class_name('User')
-    should belong_to(:invited).class_name('User')
+    it do
+      should belong_to(:event)
+      should belong_to(:host).class_name('User')
+      should belong_to(:invited).class_name('User')
+    end
 
-    should validate_presence_of(:invited_id)
-    should validate_uniqueness_of(:event_id).scoped_to(:invited_id)
+    context 'validations' do
+      it do
+        should validate_presence_of(:invited_id)
+        should validate_uniqueness_of(:event_id).scoped_to(:invited_id)
+      end
+    end
   end
 end
