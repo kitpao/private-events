30db43cdf752e6fbce82e85321f61b2effd51bf3
Eduardo <jecruxiz@gmail.com>
2020-06-19 17:52:36 -0500

    :white_check_mark: Updating test for event model
    Theres a bug in the last test should be fix

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :event do
-    
+    description {Faker::Lorem.paragraph}
+    host { host_created }
+    location {Faker::Lorem.sentence}
+    date { Time.now - rand(80000...100000)}
   end
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,9 +1,34 @@
+require 'factory_bot'
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
+   describe Event do
+      it do
+        should belong_to(:host).class_name('User')
+        should have_many(:invitations)
+        should have_many(:inviteds).through(:invitations).source(:invited)
+      end
 
+      it 'validations ' do
+        should validate_presence_of(:description)
+        should validate_uniqueness_of(:description).case_insensitive
 
+        should validate_presence_of(:location)
+        should validate_presence_of(:date)
+      end
 
+      #let(:host_created) { FactoryBot.create(:user) }
+      let(:event_created) { FactoryBot.create(:event )}
+      it 'scopes' do
 
-  pending "add some examples to (or delete) #{__FILE__}"
+        10.times do |i|
+          events_created << event_created
+        end
+        @events = events_created.upcoming_events
+
+        expect(@events).to be_empty
+        
+      end
+   end
+
 end
