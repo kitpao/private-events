08527e7463d16e6547864cf3a6bd493340b83bed
kitpao <kitpao91@gmail.com>
2020-06-22 17:35:44 -0500

    :white_check_mark: Add test for saving invitations in model

--- spec/factories/invitations.rb
+++ spec/factories/invitations.rb
@@ -1,14 +1,7 @@
 FactoryBot.define do
   factory :invitation do
-     host{nil}
-     event{nil}
-     status{}
+     host { nil }
+     event { nil }
+     invited { nil }
   end
 end
-
-=begin
-  t.integer "host_id"
-    t.integer "invited_id"
-    t.integer "event_id"
-    t.string "status", default: "pending"
-=end
--- spec/models/invitation_spec.rb
+++ spec/models/invitation_spec.rb
@@ -8,11 +8,27 @@ RSpec.describe Invitation, type: :model do
       should belong_to(:invited).class_name('User')
     end
 
-    context 'validations' do
-      it do
-        should validate_presence_of(:invited_id)
-        should validate_uniqueness_of(:event_id).scoped_to(:invited_id)
-      end
+    it 'check validations' do
+      should validate_presence_of(:invited_id)
+      should validate_uniqueness_of(:event_id).scoped_to(:invited_id)
+    end
+
+    context 'Create Invitation' do
+      #@invitation = Invitation.new(invitation_params)
+      #@invitation.host = current_user
+      #@invitation.event_id = session[:current_event]
+      let(:host) { create(:user) }
+      let(:event) { create(:event, host: host)}
+      let(:invited) { create(:user)}
+      let(:invitation_test) { Invitation.new(host: host, event: event, invited: invited) }
+
+        it 'should save invitation' do
+          expect(invitation_test.save).to eq(true)
+        end
+        it 'should not save invitation' do
+          invitation_test.host = nil
+          expect(invitation_test.save).to eq(false)
+        end
     end
   end
 end
