e10fbcdcf595c2013e0668198b961c063b9d9447
Eduardo <jecruxiz@gmail.com>
2020-06-24 14:09:07 -0500

    :bug: Fixing a bug for creating users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    @user.name.downcase!
     if @user.save
       session[:user_id] = @user.id
       flash[:success] = 'User created succesfully'
